801eb347d8d5f6be8c5db6d48b2b132dd4be24f3
eesuhn <eason.yihong@gmail.com>
2024-07-17 21:38:07 +0800

    Added test path

---
+++ src/test/scala/pokemon/model/PokemonTest.scala
@@ -0,0 +1,11 @@
+package pokemon.model
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class PokemonTest extends AnyFunSuite {
+  test("Initialize Pokemon") {
+    val charmander = new Charmander
+    val Squirtle = new Squirtle
+    val Bulbasaur = new Bulbasaur
+  }
+}
