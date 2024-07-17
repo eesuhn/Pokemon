bbf1114d29d62339e07bf49aeda724106b7510d4
eesuhn <eason.yihong@gmail.com>
2024-07-18 01:15:46 +0800

    Added modifier test case

--- src/test/scala/pokemon/model/PokemonTest.scala
+++ src/test/scala/pokemon/model/PokemonTest.scala
@@ -3,9 +3,34 @@ package pokemon.model
 import org.scalatest.funsuite.AnyFunSuite
 
 class PokemonTest extends AnyFunSuite {
-  test("Initialize Pokemon") {
-    val charmander = new Charmander
-    val Squirtle = new Squirtle
-    val Bulbasaur = new Bulbasaur
+
+  /**
+    * Test the modifier value for Scratch against Squirtle
+    */
+  test("Scratch against Squirtle") {
+    val charmander = new Charmander()
+    val squirtle = new Squirtle()
+    val modifier = charmander.calculateModifier(Scratch, squirtle)
+    assert(modifier == 1.0)
+  }
+
+  /**
+    * Test the modifier value for Ember against Squirtle
+    */
+  test("Ember against Squirtle") {
+    val charmander = new Charmander()
+    val squirtle = new Squirtle()
+    val modifier = charmander.calculateModifier(Ember, squirtle)
+    assert(modifier == 0.5)
+  }
+
+  /**
+    * Test the modifier value for Water Gun against Charmander
+    */
+  test("Water Gun against Charmander") {
+    val charmander = new Charmander()
+    val squirtle = new Squirtle()
+    val modifier = squirtle.calculateModifier(WaterGun, charmander)
+    assert(modifier == 2.0)
   }
 }
