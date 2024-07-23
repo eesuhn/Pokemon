1832a3cdd4e41af1b0a677650387d269baf502f0
eesuhn <eason.yihong@gmail.com>
2024-07-23 16:37:51 +0800

    Added res, Refactor controller

--- src/main/scala/pokemon/model/Layout.scala
+++ src/main/scala/pokemon/model/Layout.scala
@@ -1,33 +1,18 @@
 package pokemon.model
 
 import pokemon.MainApp
-import scalafxml.core.{FXMLLoader, NoDependencyResolver}
+import pokemon.util.ResourceUtil
 import javafx.{scene => jfxs}
 
 object Layout {
 
-  /**
-    * Load a resource layout from "view" folder
-    *
-    * @param path
-    * @return
-    */
-  def resourceLayout(path: String): FXMLLoader = {
-    val resource = MainApp.getClass.getResourceAsStream(s"view/${path}")
-    if (resource == null) throw new Exception(s"Cannot load resource: $path")
-
-    val loader = new FXMLLoader(null, NoDependencyResolver)
-    loader.load(resource)
-    loader
-  }
-
   def rootLayout(): jfxs.layout.BorderPane = {
-    val loader = resourceLayout("RootLayout.fxml")
+    val loader = ResourceUtil.resourceLayout("RootLayout.fxml")
     loader.getRoot[jfxs.layout.BorderPane]
   }
 
   def showGameLayout(): Unit = {
-    val loader = resourceLayout("GameLayout.fxml")
+    val loader = ResourceUtil.resourceLayout("GameLayout.fxml")
     val gameLayout = loader.getRoot[jfxs.layout.AnchorPane]
     MainApp.rootLayout.setCenter(gameLayout)
   }
--- src/main/scala/pokemon/view/GameController.scala
+++ src/main/scala/pokemon/view/GameController.scala
@@ -1,8 +1,13 @@
 package pokemon.view
 
+import pokemon.util.ResourceUtil
+import scalafx.scene.image.ImageView
 import scalafxml.core.macros.sfxml
 
 @sfxml
-class GameController() {
+class GameController(
+  val battleBg: ImageView,
+  ) {
 
+  battleBg.image = ResourceUtil.resouceImage("misc/battle-bg.gif")
 }
